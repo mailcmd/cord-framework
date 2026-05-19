fb97c76215dadeca6ed00cb1616b16b08e440487
Mauricio Santecchia <mailcmd@gmail.com>
2026-05-19 09:26:53 -0300

    .

--- lib/server/utils.ex
+++ lib/server/utils.ex
@@ -5,5 +5,58 @@ defmodule CORD.Utils do
       {k,v} when is_binary(k) -> {String.to_atom(k), v}
       {k,v} -> {k,v}
     end) |> Enum.into(%{})
+  end
+
+  def httpc_get(url, headers) do
+    headers = Enum.map(headers, fn {k, v} -> {to_charlist(k), to_charlist(v)} end)
+    http_request_opts = [
+      ssl: [
+        verify: :verify_peer,
+        customize_hostname_check: [
+          match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
+        ],
+        cacerts: :public_key.cacerts_get()
+      ]
+    ]
+
+    case :httpc.request(:get, {to_charlist(url), headers}, http_request_opts, []) do
+      {:ok, {{_, code, _}, resp_headers, resp_body}} ->
+        {:ok, {
+          code,
+          Enum.map(resp_headers, fn {k, v} -> {to_string(k), to_string(v)} end),
+          to_string(resp_body)
+        }}
+      {:error, error} ->
+        {:error, error}
+    end
+  end  
+
+  def httpc_post(url, headers, body, content_type) do
+    headers = Enum.map(headers, fn {k, v} -> {to_charlist(k), to_charlist(v)} end)
+    http_request_opts = [
+      ssl: [
+        verify: :verify_peer,
+        customize_hostname_check: [
+          match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
+        ],
+        cacerts: :public_key.cacerts_get()
+      ]
+    ]
+
+    case :httpc.request(
+           :post,
+           {to_charlist(url), headers, to_charlist(content_type), to_charlist(body)},
+           http_request_opts,
+           []
+         ) do
+      {:ok, {{_, code, _}, resp_headers, resp_body}} ->
+        {:ok, {
+          code,
+          Enum.map(resp_headers, fn {k, v} -> {to_string(k), to_string(v)} end),
+          to_string(resp_body)
+        }}
+      {:error, error} ->
+        {:error, error}
+    end
   end  
 end
--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule CORD.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :inet],
       mod: {CORD.Application, []}
     ]
   end
